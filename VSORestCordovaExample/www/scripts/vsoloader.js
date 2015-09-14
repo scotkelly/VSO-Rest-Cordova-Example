8385df53e7a0579e4b0b89d12a2f52fc5cb20926
Scot Kelly <scot_kelly@hotmail.com>
2015-09-13 19:51:44 -0700

    Basic functioning project.

---
+++ VSORestCordovaExample/www/scripts/vsoloader.js
@@ -0,0 +1,24 @@
+﻿function loadProjects() 
+{
+    var username = ""; // put username to use here
+    var password = ""; // put personal access token here
+
+    $.ajax({
+        type: "GET",
+        datatype: "json",
+        url: "https://rapid.visualstudio.com/DefaultCollection/_apis/projects?api-version=1.0",
+        
+        headers: {
+            "Authorization": "Basic " + btoa(username + ":" + password)
+        },
+
+    }).done(function (projects) {
+        var projectNames = "";
+        for (var i = 0; i < projects.value.length; i++) {
+            projectNames += projects.value[i].name + "\n";
+        }
+        
+        alert(projectNames);
+
+        });
+}
