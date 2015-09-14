b0c3d0eb88a3df862bbfb6caa62f34281bf2438e
Scot Kelly <scot_kelly@hotmail.com>
2015-09-13 20:23:51 -0700

    Updated example to allow user to input values for the user name to login with, the personal access token (or alt. credentials), and account name.

--- VSORestCordovaExample/www/scripts/vsoloader.js
+++ VSORestCordovaExample/www/scripts/vsoloader.js
@@ -1,24 +1,22 @@
-﻿function loadProjects() 
+﻿function loadProjects(username, accesstoken, elementToAppendValuesTo, accountName) 
 {
-    var username = ""; // put username to use here
-    var password = ""; // put personal access token here
-
     $.ajax({
         type: "GET",
         datatype: "json",
-        url: "https://rapid.visualstudio.com/DefaultCollection/_apis/projects?api-version=1.0",
+        url: "https://" + accountName + "/DefaultCollection/_apis/projects?api-version=1.0",
         
         headers: {
-            "Authorization": "Basic " + btoa(username + ":" + password)
+            "Authorization": "Basic " + btoa(username + ":" + accesstoken)
         },
 
     }).done(function (projects) {
-        var projectNames = "";
+        projectNames = "";
         for (var i = 0; i < projects.value.length; i++) {
-            projectNames += projects.value[i].name + "\n";
-        }
-        
-        alert(projectNames);
+            projectNames += "<P>" + projects.value[i].name + "</P>";
+        }              
+        elementToAppendValuesTo.append(projectNames);
+    }).fail(function () {
+        elementToAppendValuesTo.append("Project list did not load. :(");
+    });
 
-        });
 }
